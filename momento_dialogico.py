f0b1ae067caf5561b2f08440d6f986bab26e1213
Your Name <you@example.com>
2025-11-22 22:05:41 +0100

    Add data for index creation

--- momento_dialogico.py
+++
@@ -1,8 +0,0 @@
-from momento_legame import calcolo_momento_legame
-
-def calcolo_momento_dialogico(analisi):
-    momento_legame = calcolo_momento_legame(analisi)
-    momento_dialogico = momento_legame.sum()
-    
-    return momento_dialogico
-    
